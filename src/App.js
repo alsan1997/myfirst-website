63a8dffca8aed2f04cea6f57508d77b392ed2a70
Aldo Sanjoto <aldosanjoto@gmail.com>
2018-04-08 17:42:12 -0500

    aggregate
    changes updated to the aggregates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
+// 6f2c8665b3fc4ed497b744ea39955d26 id
+// 4a9ae37d5a414b2c9bb6620d714e9e6e secret
 
-class Aggregate extends Component{
+let hardCodedData ={
+	user:{
+		name: 'Aldo',
+		playlist: [
+			{
+				name:'My favorites',
+				songs: [
+				  {name: 'Hello World', duration: 1230}, 
+				  {name: 'Do not leave me', duration: 1211}, 
+				  {name: 'Rossa', duration:7299}
+				]
+			},
+			{
+				name: 'Discover weekly',
+				songs:[
+				  {name: 'Le song', duration: 2121}, 
+				  {name: 'The song', duration: 3321}, 
+				  {name: 'Slide', duration: 8992}
+				]
+			},
+			{
+				name: 'Rappish',
+				songs:[
+				  {name: 'Eminem', duration: 3121}, 
+				  {name: 'Not afraid', duration: 8821}
+				]
+			}
+		]
+	}
+}
+
+class AggregatePlaylist extends Component{
   render(){
     return(
       <div style={{width:"40%", display: 'inline-block'}} className="Aggregate">
-        <h2 style = {{color: '#fff2'}}> number Text </h2>
+        <h2 style = {{color: '#fff2'}}> {this.props.playlists.length} playlists </h2>
+      </div>
+    );
+  }
+}
+
+class AggregateHour extends Component{
+  render(){
+  	let allSongs = this.props.playlists.reduce((songs, eachPlaylist) =>{
+  		return songs.concat(eachPlaylist.songs)
+  	}, [])
+  	let totalDuration = allSongs.reduce((sum, eachSong)=>{
+  		return sum + eachSong.duration
+  	}, 0)
+
+    return(
+      <div style={{width:"40%", display: 'inline-block'}} className="Aggregate">
+        <h2 style = {{color: '#fff2'}}> {Math.round(totalDuration / 3600)} hours </h2>
       </div>
     );
   }
@@ -42,14 +92,34 @@ class Playlist extends Component{
 }
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state = {serverData: {}}
+	}
+
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({serverData: hardCodedData});
+		}, 1000)
+	}
+
   render() {
     return (
        <div className="App">
-         <h1> mySpotify </h1>
-         <Aggregate/>
-         <Aggregate/>
-         <Search/>
-         <Playlist/>
+         {
+         this.state.serverData.user ? 
+         <div>
+           <h1> {this.state.serverData.user.name} mySpotify </h1>
+ 
+           <AggregatePlaylist playlists={this.state.serverData.user.playlist}/>
+         	 <AggregateHour playlists={this.state.serverData.user.playlist}/>
+         
+           <Search/>
+           <Playlist/>
+           <Playlist/>
+           <Playlist/>
+         </div> : <h2> Loading your Spotify profile... </h2>
+         }
        </div>
     );
   }
