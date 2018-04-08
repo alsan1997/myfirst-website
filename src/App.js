11507bf38dc693a79443202da63532eae563fa70
Aldo Sanjoto <aldosanjoto@gmail.com>
2018-04-07 20:55:47 -0500

    initial commit
    copied initial files

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Aggregate extends Component{
+  render(){
+    return(
+      <div style={{width:"40%", display: 'inline-block'}} className="Aggregate">
+        <h2 style = {{color: '#fff2'}}> number Text </h2>
+      </div>
+    );
+  }
+}
+
+class Search extends Component{
+  render(){
+    return(
+      <div>
+        <img/>
+        <input type="text"/>
+        Search
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component{
+  render(){
+    return(
+      <div style={{width: "25%"}}>
+        <img/>
+        <h3> Playlist name </h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+       <div className="App">
+         <h1> mySpotify </h1>
+         <Aggregate/>
+         <Aggregate/>
+         <Search/>
+         <Playlist/>
+       </div>
+    );
+  }
+}
+
+export default App;
