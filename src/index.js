f561c5e4740e4d5b6cb2e0ee7f5f4d9c2737337d
Aldo Sanjoto <aldosanjoto@gmail.com>
2018-04-15 19:42:17 -0500

    bootstrap
    navbar spacing

--- src/index.js
+++ src/index.js
@@ -10,29 +10,26 @@ import Playlist from './Playlist';
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<Router>
-		<div className="container">
-
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a className="navbar-brand" href="#">Spoticool</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active"><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/login">Login</NavLink></li>
-          <li className="nav-item"><NavLink to="/playlist">Playlist</NavLink></li>
-        </ul>
-         
-      </div>
-    
-    </nav>
+  <Router>
+    <div className="container">
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a className="navbar-brand" href="#">Spoticool</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+          <li><NavLink exact className="nav-link" to="/">Home</NavLink></li>
+          <li><NavLink className="nav-link"to="/login">Login</NavLink></li>
+          <li><NavLink className="nav-link" to="/playlist">Playlist</NavLink></li>
+          </ul>         
+        </div>   
+      </nav>
     <Route exact path ="/" component={Home}/>
     <Route path="/playlist" component={Playlist}/>
     <Route path="/login" component={Login}/>
 
-    </div>
+  </div>
   </Router>, 
-	document.getElementById('root'));
+  document.getElementById('root'));
 //registerServiceWorker();
